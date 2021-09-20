dedfeddd90c7713b8d64a8a3bebc0c5cc57b5ac2
Janu4 <akmalnafis25@gmail.com>
2021-09-20 23:10:02 +0700

    Update NO 3.c

--- ch 3/NO 3.c
+++ ch 3/NO 3.c
@@ -14,89 +14,75 @@ luas permukaan kerucut melingkar kanan dan menampilkannya kembali kepada penggun
 digunakan dalam persamaan untuk menghitung luas permukaan total dan karenanya, Anda dapat:
 harus menggunakan nilai yang sesuai untuk itu.
 /*  Problem: 
-            Dibutuhkan program untuk menghitung total luas permukaan kerucut melingkar 
+            Dibutuhkan program untuk menghitung total luas permukaan kerucut melingkar kanan 
             dan menampilkannya kembali kepada pengguna.
     Analysis:
-            Untuk menghitung total luas permukaan kerucut melingkar maka diperlukan
+            Untuk menghitung total luas permukaan kerucut melingkar maka diperlukan formula persamaan lingkaran
+	    dengan menggunakan phi di dalamnya. input berupa jari-jari alas lingkaran,
+	    kerucut dan tinggi kemiringan kerucut.
 
     Data Requirement:
-    
-            Problem Inputs:		
-            int total_num_of_payments	// jumlah pembayaran
-	    double purchase_price	// harga pembelian
-	    double down_payment		// uang muka
-	    double annual_interest_rate		// bunga tahunan
-	    double principle
-	 
+    	
+	PI 3.14159265					//konstanta PHI
+	double surface_area(double slant_height, double radius) 		//membuat fungsi tambahan sebagai tempat untuk rumus
+	double area;
             
+	    Problem Inputs:		
+	    double slant_height;			//tinggi kemiringan kerucut
+	    double radius;				// jari jari alas kerucut
+	   
             Problem Outputs:
-	    
-	    double amount_borrowed	// uang yang dipinjam
-	    double monthly_payment       // pembayaran bulanan
+	    double surface;				//luas permukaan kerucut
 	    
             Relevant Formulas:
-            payment(double P, double i, int n) {			//fungsi dari rumus untuk menghitung pembayaran
-	    return ((i * P) / (1 - pow(1 + i, -1 * (double)n))););
-	    
-	    monthly_interest_rate = annual_interest_rate / 12;  //  bunga bulanan
-	    amount_borrowed = purchase_price - down_payment;   // menghitung uang yang dipinjam
-	
+            area = PI * radius * (radius + slant_height);
 */
 /*  Initial Algoritm:
     1. menampilkan apa yang harus diinput  
-    2. memasukkan input berupa harga harga pembelian, uang muka pembayaran, tingkat bunga tahunan, dan jumlah pembayaran.
-    3. menghitung bunga bulanan
-    4. menghitung jumlah uanng yang dipinjam
-    5. menghitung pembayaran perbulan
+    2. memasukkan input berupa erupa jari-jari alas lingkaran kerucut dan tinggi kemiringan kerucut.
+    3. menghitung luas permukaan kerucut
+    4. menampilkan hasil 
    
     
    Implementation
    
-   Untuk mendapat data banyak siswa yang terdaftar, maka perlu Input dari user
+   Untuk mendapat data jari-jari alas lingkaran kerucut dan tinggi kemiringan kerucut.
    sebagai berikut:
-            printf("Enter the number of students enrolled: ");
-            scanf("%d", &peserta_terdaftar);
+  	printf("Masukkan jari jari alas lingkaran kerucut (cm) = ");
+  	scanf("%lf", &radius);
+
+  	printf("masukkan tinggi kemiringan (cm) = ");
+  	scanf("%lf", &slant_height);
     
-   Untuk menghitung bagian yang diperlukan dan jumlah siswa yang tersisa digunakan statment 
-            bagian_diperlukan = peserta_terdaftar/ SISWA_TIAP_BAGIAN ;
-            siswa_yang_tersisa = peserta_terdaftar % SISWA_TIAP_BAGIAN ;
-   Setelah itu maka ditampilkan jumlah siswa yang terdaftar, bagian yang diperlukan, dan jumlah siswa yang tersisa:
-            printf("Dengan %d Siswa terdaftar, Bagian yang diperlukan: %d\nSiswa yang tersisa: %d\n",
-            peserta_terdaftar ,bagian_diperlukan, siswa_yang_tersisa);
+   Untuk menghitung luas permukaan kerucut menggunakan formula digunakan statment 
+             area = PI * radius * (radius + slant_height);
+   Setelah untuk menampilkan  luas permukaan kerucut digunakan statment 
+            	surface = surface_area(slant_height, radius);
+		printf("luas permukaan kerucut adalah: %.2lf cm²\n", surface);
     */
 #include <stdio.h>
 #include <stdio.h>
 
-// 1. define pi as it's a constant
 #define PI 3.14159265
 
-// 2. create a function named surface_area() the takes slant_height and
-//    radius of the base as the arguments
 double surface_area(double slant_height, double radius) {
   double area;
-
-  // 3. plug all the arguments into the mentioned formula and return the result
   area = PI * radius * (radius + slant_height);
 
   return area;
 }
 
-// 4. create a main function that prompts user to enter slant height and
-//    radius
 int main() {
   double slant_height, radius, surface;
 
-  printf("enter radius of a cone's circular base (cm) => ");
+  printf("Masukkan jari jari alas lingkaran kerucut (cm) = ");
   scanf("%lf", &radius);
 
-  printf("enter slant height of a cone (cm) => ");
+  printf("masukkan tinggi kemiringan (cm) = ");
   scanf("%lf", &slant_height);
 
-  // 5. call the surface_area() with the entered values and print
-  //    the result out
   surface = surface_area(slant_height, radius);
-
-  printf("surface area of the cone is: %.2lf cm²\n", surface);
+	printf("luas permukaan kerucut adalah: %.2lf cm²\n", surface);
 
   return 0;
 }

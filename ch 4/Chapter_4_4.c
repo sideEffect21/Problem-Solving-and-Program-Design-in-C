0b169720575431d7dbfa3082fa2d3b415ceca77c
Janu4 <akmalnafis25@gmail.com>
2021-09-27 07:57:07 +0700

    Update Chapter_4_4.c

--- ch 4/Chapter_4_4.c
+++ ch 4/Chapter_4_4.c
@@ -1,3 +1,23 @@
+
+// Problem
+/*
+program yang melaporkan isi tabung gas terkompresi pada huruf pertama dari warna silinder. Masukan program berupa karakter yang mewakili 
+warna silinder yang diamati: "Y" atau "y" untuk kuning, "O" atau "o" untuk orange, dan sebagainya. jika input tidak sesuai maka output "Contents unknown".
+*/
+
+// Analysis
+/*
+	Program  ini membutuhkan input berupa huruf pertama baik huruf kecil tau besar bernilai sama yang menunjukkan 
+	warna slinder dari tabung:
+	
+	orange menunjukkan ammonia
+	brown menunjukkan carbon monoxide
+	yellow menunjukkan hydrogen
+	green menunjukkan oxygen
+	kita dapat menggunakn operasi == untuk mengecek warna yang benar dan menggunkan operasi || untuk menyamakan nilai huruf kecil atau besar
+	
+	jika input sesui maka kan menampilkn warna dan isi dalam tabung tersebut, jika salah maka menampilkan Contents Unknown
+	*/
 #include "stdio.h"
 void main()
 {

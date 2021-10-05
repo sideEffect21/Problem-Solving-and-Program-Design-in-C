d1694ad3c5afaeefeafb41d3dea4d95fc429a9a4
Janu4 <akmalnafis25@gmail.com>
2021-10-06 06:29:15 +0700

    Update Chapter_5_6.c

--- ch 5/Chapter_5_6.c
+++ ch 5/Chapter_5_6.c
@@ -11,77 +11,62 @@
 
 // Problem
 /*
-Klub Kesadaran Lingkungan BigCorp International mengusulkan
-bahwa perusahaan mensubsidi $0,08 per penumpang-kilometer perjalanan
-biaya karyawan yang membentuk carpools yang memenuhi minimum yang ditentukan
-efisiensi pembangkit. Efisiensi penumpang P (dalam penumpang-kilometer per liter) adalah
-didefinisikan sebagai
-P 5ns
-aku
- di mana n adalah jumlah penumpang, s adalah jarak yang ditempuh dalam kilometer,
-dan l adalah jumlah liter bensin yang digunakan. Tulis program yang memproses file input data pada carpool yang ada
-(carpool.txt), membuat file output effic.txt yang berisi tabel semua
-carpools yang memenuhi minimum efisiensi penumpang. File input dimulai
-dengan angka yang merupakan efisiensi penumpang minimum. Setiap carpool diwakili
-dibenci oleh baris data yang berisi tiga angka: jumlah orang di
-carpool, total jarak perjalanan per lima hari seminggu, dan jumlah
-liter bensin yang dikonsumsi dalam seminggu perjalanan. File data diakhiri dengan
-garis nol. Tulis hasil Anda dengan format ini:
+Tulis program yang memproses file input data pada carpool yang ada
+didata yang tersedia , membuat file output banyak mobil dengan penumpang yang sesuai efisiensi. 
 
 */
 
 // Analysis
 /*
-untuk membuat program tersebut kita harus mengecek angka tersebut dengan pembagi nol
-kemudian hitung sisa pembagi lama dibagi sisanya ditemukan. ulangi proses ini sampai sisanya nol
-	
+untuk menghitung dan mencari tempat parkir yang sesuai efisiensi penumpang di masukkan input
+File input dimulai dengan angka yang merupakan efisiensi penumpang minimum. Setiap carpool diwakili
+dibenci oleh baris data yang berisi tiga angka: jumlah orang di
+carpool, total jarak perjalanan per lima hari seminggu, dan jumlah
+liter bensin yang dikonsumsi dalam seminggu perjalanan. File data diakhiri dengan
+garis nol. dengan format yang sesuai 
+
 // Data Requirements
 /*
 	Problem inputs
-	int a, b;			//input angka
-	
+	passengers, distance, gasoline	
 	
 	Problem outputs
-int	r, g;		//otput yang keluar
+
+	efficiency subs		
 	
+	relevant formula 
+	efficiency = (passengers * distance) / gasoline;
+		subs = passengers * distance * SUBS;
 */
 
 // Design
 /*
 	Initial algorithm
-	1. meminta 2 data angka 
-	2. mengecek kedua input 
-	3. mengecek apakah bilangan tersebut dibagi sisanya nol atau tidak
-	4. jika tidak program tersebut akan mengulang dan membagi sampai nol
+	1. meminta input banyak penumpang, bahan bakar yang dipakai yang jarak.
+	2. menghitung dan mengecek efisiensi dan penumpang
+	3. data akan dimasukkan dalam fungsi
+	4. menampilkan tempat parkir yang sesuai
 // Implementations
 /*
-	Pertama, kita perlu meminta input berupa input
-  printf("\ngcd(a, b)");
-	printf("\nEnter a - ");
-	scanf("%d", &a);
-	printf("\nEnter b - ");
-	scanf("%d", &b);
-  
-	Setelah itu, menggunakan staement if untuk mengecek angkanya
-	if((a % b) == 0)
-		g = b;
-	
-jika bernulai false maka akan dilakukan perulangan
- else
-	{	
-		r = a % b;
-		while(r != 0)
-		{
-			a = b;
-			b = r;
-			r = a % b;
-		}
-		g = b;
+melakukan perulangan untuk mencari mobil yang sesuai tempat parkir dari data yang di berikan 
+
+	while((input_status = scanf("%d%lf%lf", &passengers, &distance, &gasoline)) != EOF)
+	{
+		if(passengers == 0)		// jika penumpang 0 berhenti
+			break;
+		efficiency = (passengers * distance) / gasoline;
+		subs = passengers * distance * SUBS;		//mencari subsidi 
+		print_chart(passengers, efficiency, gasoline, distance, subs);
 	}
-  
-  untuk menampilkan hasil 
-  printf("\ngcd(a, b) - %d\n", g);
+	
+  	kemudian data tersebut ditampilkan 
+	
+	print_chart(int passengers, double efficiency, double gasoline, double distance, double subs)
+{
+	printf("\n%d %15c %.2f %18c %.2f %20c %.2f %20c %.2f", passengers, ' ', distance, ' ', gasoline, ' ', efficiency, ' ', subs);
 }
+	
+
 	*/
 #include "stdio.h"
 #define SUBS 0.08
@@ -91,9 +76,9 @@ void main()
 	double efficiency, gasoline, distance, subs;
 	int passengers, input_status;
 	
-	printf("%5cCARPOOLS MEETING MINIMUM PASSENGER EFFICIENCY OF 25 PASSENGER KM/L", ' ');
+	printf("%5ctempat parkir minimal efisiensi kendaraan dalam 25km/jam", ' ');
 	
-	printf("\nPassengers %3c Weekly Commute (km) %3c Gasoline Consumption(L) %3c Efficiency (pass km/L) %3c Weekly Subsidy($)", ' ', ' ', ' ', ' ');
+	printf("\nPenumpang %3c perjalanan mingguan (km) %3c gasoline yang dikonsumsi(L) %3c Efficsiensi(pass km/L) %3c subsidi mingguan($)", ' ', ' ', ' ', ' ');
 	
 	while((input_status = scanf("%d%lf%lf", &passengers, &distance, &gasoline)) != EOF)
 	{
@@ -103,7 +88,6 @@ void main()
 		subs = passengers * distance * SUBS;
 		print_chart(passengers, efficiency, gasoline, distance, subs);
 	}
-	
 }
 
 void print_chart(int passengers, double efficiency, double gasoline, double distance, double subs)
